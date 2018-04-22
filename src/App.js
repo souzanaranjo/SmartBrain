c224d39aa8abe5ecadd3f8b01e2d350fbaf6b915
David Souza <souzanaranjo@gmail.com>
2018-04-22 18:27:49 -0500

    Enabled the face recognition feature with the Clarifai API, also shows the picture when submitting the picture url

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: 'd3c4a65201954ff99d66e8b782c783fd'
+});
 
 const particlesOptions = {
   particles: {
@@ -19,6 +25,34 @@ const particlesOptions = {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl:''
+    }
+  }
+
+onInputChange = (event) => {
+  this.setState({input: event.target.value});
+}
+
+onButtonSubmit = () => {
+  this.setState({ imageUrl: this.state.input });
+  app.models
+  .predict(
+    Clarifai.FACE_DETECT_MODEL,
+    this.state.input)
+  .then(
+    function(response) {
+      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+    },
+    function(err) {
+      console.log(err)
+    }
+  );
+}
+
   render() {
     return (
       <div className="App">
@@ -28,9 +62,10 @@ class App extends Component {
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
-      {/*
-      <FaceRecognition />*/}
+      <ImageLinkForm 
+      onInputChange={this.onInputChange} 
+      onButtonSubmit={this.onButtonSubmit} />
+      <FaceRecognition imageUrl={ this.state.imageUrl }/>
       </div>
     );
   }
