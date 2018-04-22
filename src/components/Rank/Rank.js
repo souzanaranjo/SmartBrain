d6701d9c4203890ef0199bf9f1ca9903eb474f2a
David Souza <souzanaranjo@gmail.com>
2018-04-21 19:52:18 -0500

    Created the basic front-end of the application

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Rank = () => {
+	return (
+		<div>
+			<div className='white f3'>
+				{'David, your current rank is...'}
+			</div>
+			<div className='white f1'>
+				{'#1'}
+			</div>
+			<div className='white f3'>
+				{'Winning, as always.'}
+			</div>
+		</div>
+	);
+}
+
+export default Rank;
